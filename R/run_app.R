6efcdaee8f51e63378089644b0ca38776817eaad
sebsilas <sebsilas@gmail.com>
2023-05-31 10:18:52 +0200

    Updates based on meeting

--- R/run_app.R
+++ R/run_app.R
@@ -15,31 +15,47 @@ run_app <- function(app_name = "sing-mpi") {
                                   arrhythmic = get_config('no_melodies'),
                                   rhythmic = get_config('no_melodies')
                                   ),
+                 arrhythmic_item_bank = Berkowitz_easy,
+                 rhythmic_item_bank = Berkowitz_easy,
                  feedback = FALSE,
-                 SNR_test = FALSE,
-                 get_range = FALSE,
+                 max_goes = 2L,
                  musicassessr_aws = get_config('musicassessr_aws'),
-                 skip_setup = get_config('skip_setup'),
                  examples = get_config('no_examples'),
                  melody_length=c(4, 10),
+                 SNR_test = TRUE,
+                 headphones_test = FALSE,
+                 get_user_info = FALSE,
+                 requirements_page = FALSE,
                  test_name = "Sing MPI",
                  gold_msi = FALSE,
                  demographics = FALSE,
-                 default_range = list(bottom_range = 48, top_range = 60),
+                 final_results=FALSE,
+                 get_range = FALSE,
+                 default_range = list(bottom_range = 60, top_range = 72),
                  long_tone_paradigm = "call_and_response",
-                 append_trial_block_after = extra_mpi()
+                 volume_meter_on_melody_trials = TRUE,
+                 volume_meter_on_melody_trials_type = 'playful',
+                 append_trial_block_before = extra_mpi_beginning(),
+                 append_trial_block_after = extra_mpi_end(),
+                 long_tone_length = 3,
+                 allow_SNR_failure = TRUE
                  )
 
 }
 
 
-extra_mpi <- function() {
+extra_mpi_beginning <- function() {
+  psychTestR::join(
+    sing_hbd_page(),
+    sing_all_meine_entchen_page()
+  )
+}
+
+extra_mpi_end <- function() {
 
   psychTestR::join(
 
-    sing_hbd_page(),
-
-    sing_brother_john_page(),
+    sing_brother_john_pages(),
 
     # Sing your favourite song
     musicassessr::record_audio_page(page_title = "Sing your favourite song!",
@@ -53,17 +69,19 @@ extra_mpi <- function() {
 
 
 
-sing_brother_john_page <- function() {
+sing_brother_john_pages <- function() {
 
-  musicassessr::present_stimuli(stimuli = itembankr::str_mel_to_vector(brother_john$abs_melody),
-                                durations = itembankr::str_mel_to_vector(brother_john$durations),
-                                page_title = "Sing the song!",
-                                page_text = 'When you are ready, click "Record" and sing back the song you hear.',
-                                stimuli_type = "midi_notes",
-                                display_modality = "auditory",
-                                page_type = "record_audio_page",
-                                page_label = "sing_bj",
-                                get_answer = musicassessr::get_answer_pyin)
+  purrr::pmap(brother_john, function(abs_melody, durations) {
+    musicassessr::present_stimuli(stimuli = itembankr::str_mel_to_vector(abs_melody),
+                                  durations = itembankr::str_mel_to_vector(durations),
+                                  page_title = "Sing the song!",
+                                  page_text = 'When you are ready, click "Record" and sing back the song you hear.',
+                                  stimuli_type = "midi_notes",
+                                  display_modality = "auditory",
+                                  page_type = "record_audio_page",
+                                  page_label = "sing_bj",
+                                  get_answer = musicassessr::get_answer_pyin)
+  })
 }
 
 sing_hbd_page <- function() {
@@ -76,6 +94,20 @@ sing_hbd_page <- function() {
                                 display_modality = "auditory",
                                 page_type = "record_audio_page",
                                 page_label = "sing_hbd",
+                                get_answer = musicassessr::get_answer_pyin)
+}
+
+
+sing_all_meine_entchen_page <- function() {
+
+  musicassessr::present_stimuli(stimuli = itembankr::str_mel_to_vector(all_meine_entchen$abs_melody),
+                                durations = itembankr::str_mel_to_vector(all_meine_entchen$durations),
+                                page_title = "Sing The Ugly Ducklings!",
+                                page_text = 'When you are ready, click "Record" and sing back The Ugly Ducklings after you hear it.',
+                                stimuli_type = "midi_notes",
+                                display_modality = "auditory",
+                                page_type = "record_audio_page",
+                                page_label = "sing_ducklings",
                                 get_answer = musicassessr::get_answer_pyin)
 }
 
